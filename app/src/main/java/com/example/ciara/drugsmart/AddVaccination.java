99e17f7fd67363ed93d74c42687015f680d8df7a
Ciara <115406878@umail.ucc.ie>
2018-11-18 19:05:56 +0000

    Iteration2

--- app/src/main/java/com/example/ciara/drugsmart/AddVaccination.java
+++ app/src/main/java/com/example/ciara/drugsmart/AddVaccination.java
@@ -1,9 +1,17 @@
 package com.example.ciara.drugsmart;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -28,6 +36,7 @@ public class AddVaccination extends AppCompatActivity {
     TextView animalID;
     TextView animalTag;
     Button buttonAddVaccination;
+    Button buttonAddReminder;
     Spinner vaccinationDrug;
     EditText vaccinationAdmin;
     EditText vaccinationDosage;
@@ -47,7 +56,7 @@ public class AddVaccination extends AppCompatActivity {
 
         vaccinationDrug = (Spinner)findViewById(R.id.spinnerDrug);
         vaccinationAdmin = (EditText)findViewById(R.id.editAdmin);
-        vaccinationDosage = (EditText)findViewById(R.id.editDosage);
+        vaccinationDosage = (EditText) findViewById(R.id.editDosage);
 
 
         animalID = (TextView)findViewById(R.id.TextViewAnimalID);
@@ -67,7 +76,41 @@ public class AddVaccination extends AppCompatActivity {
         buttonAddVaccination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Calling the addVaccination method to add vaccination record to the database
                 addVaccination();
+                //Also adding a notification when a vaccination is added
+
+
+
+
+            }
+        });
+
+        buttonAddReminder = (Button) findViewById(R.id.buttonAddReminder);
+        buttonAddReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Another way attempted
+                //addNotification();
+
+                //https://www.youtube.com/watch?v=1fV9NmvxXJo
+                //Time based reminder
+                Calendar calendar = Calendar.getInstance();
+
+                calendar.set(Calendar.HOUR_OF_DAY, 10);
+                calendar.set(Calendar.MINUTE, 55);
+
+                Intent intent = new Intent(getApplicationContext(), NotificationService.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+                AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+                //getTimeInMillis - this is when the notification will be triggered (if statement for conditional reminder??
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+
             }
         });
 
@@ -120,9 +163,9 @@ public class AddVaccination extends AppCompatActivity {
         String idAnimal = animalID.getText().toString().trim();
         String tag = animalTag.getText().toString().trim();
         String drug = vaccinationDrug.getSelectedItem().toString();
-        String date = vaccinationDate.toString().trim();
-        String admin = vaccinationAdmin.toString().trim();
-        String dosage = vaccinationDosage.toString().trim();
+        String date = vaccinationDate.getText().toString().trim();
+        String admin = vaccinationAdmin.getText().toString().trim();
+        String dosage = vaccinationDosage.getText().toString().trim();
 
         if(!TextUtils.isEmpty(tag)){
 
@@ -137,6 +180,37 @@ public class AddVaccination extends AppCompatActivity {
         } else{
             Toast.makeText(this, "Please select admin", Toast.LENGTH_LONG).show();
         }
+    }
+
+
+    //Creates and displays notification
+
+    private void addNotification(){
+
+       /* //https://www.youtube.com/watch?v=ATERxKKORbY
+        //Builds notification
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Vaccination")
+                .setContentText("Your animal will be out of the withdrawal period in 2 weeks");
+
+        //intent needed to display the notification
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(
+                contentIntent
+        );
+
+        //Adds notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());*/
+
+
+
+
+
+
     }
 
 }
